5e359b79bdd07634bd338e7768b1bdcef644efe2
Maicon Souza <maicon_souza09@hotmail.com>
2025-05-20 07:32:12 -0300

    20-05-25

---
+++ curso_python/aula1.py
@@ -0,0 +1,8 @@
+# Comentário de uma linha
+print("123")  # na frente
+# Abaixo
+
+"""
+Docstring não é um comentário
+O interpretador do python vai ler essa linha ao contrario do comentário com "#"
+"""
