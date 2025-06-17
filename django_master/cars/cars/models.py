a956dc968c1ec06149fd7269b73a3e2239f5089b
Maicon Souza <maicon_souza09@hotmail.com>
2025-06-16 22:36:52 -0300

    16-06-25 22:36

--- django_master/cars/cars/models.py
+++ django_master/cars/cars/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    model_year = models.IntegerField(max_length=4, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
