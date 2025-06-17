39adb811ceb99c19ede7712706265d71ef79d0b5
Maicon Souza <maicon_souza09@hotmail.com>
2025-06-17 06:58:28 -0300

    17-06-25 06:58

--- django_master/cars/cars/admin.py
+++ django_master/cars/cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Car
 # Register your models here.
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'model_year', 'value', 'plate')
+    search_fields = ('model', 'brand')
+
+
+# admin.site.register(Car, CarAdmin)
--- django_master/cars/cars/models.py
+++ django_master/cars/cars/models.py
@@ -7,6 +7,6 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     brand = models.CharField(max_length=200)
-    model_year = models.IntegerField(max_length=4, blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
